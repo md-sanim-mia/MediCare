389d46705b5f3d28818a791e4514e4aa99244c79
SanimSRk <hasansanim562@gmail.com>
2025-03-05 11:22:02 +0600

    chenge some desing

--- src/components/shared/Naver.tsx
+++ src/components/shared/Naver.tsx
@@ -1,6 +1,7 @@
 "use client";
-import { Heart, ShoppingBag } from "lucide-react";
+import { Heart, LogOut, Search, ShoppingBag } from "lucide-react";
 import { Button } from "../ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -9,7 +10,6 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "../ui/dropdown-menu";
-import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import Link from "next/link";
 import { useState } from "react";
 import logo from ".//..//..//assets/v987-18a-removebg-preview.png";
@@ -42,8 +42,8 @@ const Naver = () => {
       {" "}
       <nav className=" h-16 fixed  z-40 w-full mx-auto  bg-white shadow dark:bg-gray-800">
         <div className="max-w-screen-xl px-4 py-3 grid items-center mx-auto">
-          <div className="lg:flex lg:items-center lg:justify-between">
-            <div className="flex items-center justify-between">
+          <div className="lg:flex lg:items-center  lg:justify-between">
+            <div className="flex items-center justify-between ">
               <div className="flex gap-2 items-center">
                 <Image
                   className="text-white"
@@ -58,7 +58,34 @@ const Naver = () => {
                 </h2>
               </div>
 
-              <div className="flex lg:hidden gap-3 items-center">
+              <div className="flex lg:hidden gap-6 items-center">
+                <div className="flex lg:hidden gap-4 items-center">
+                  <Heart />
+                  <ShoppingBag />
+                  <Search />
+                  <div className="ml-4">
+                    <DropdownMenu>
+                      <DropdownMenuTrigger>
+                        <Avatar>
+                          <AvatarImage src="https://github.com/shadcn.png" />
+                          <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent>
+                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>Profile</DropdownMenuItem>
+                        <DropdownMenuItem>Dashboard</DropdownMenuItem>
+                        <DropdownMenuItem>My Shop</DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem className="bg-red-500 text-gray-100 cursor-pointer">
+                          <LogOut />
+                          Log Out
+                        </DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </div>
+                </div>
                 <div className="">
                   <button
                     onClick={() => setIsOpen(!isOpen)}
@@ -99,7 +126,6 @@ const Naver = () => {
                     )}
                   </button>
                 </div>
-                <div>kfdsla</div>
               </div>
             </div>
 
@@ -110,88 +136,49 @@ const Naver = () => {
                   : "opacity-0 -translate-x-full"
               }`}
             >
-              <div className="flex flex-col -mx-6 lg:flex-row lg:items-center  lg:mx-8">
+              <div className="flex flex-col  lg:flex-row lg:items-center  ">
                 {navItems}
+              </div>
+              <div className="lg:flex hidden">
                 <Button
                   variant="outline"
-                  className="rounded-full mx-3 p-0 size-10"
+                  className="rounded-full mx-2 p-0 size-9"
                 >
                   <Heart />
                 </Button>
                 <Button
                   variant="outline"
-                  className="rounded-full mx-3 p-0 size-10 "
+                  className="rounded-full mx-2 p-0 size-9 "
                 >
                   <ShoppingBag />
                 </Button>
-              </div>
-              <div className="lg:grid hidden">
-                <div className="relative inline-block">
-                  {/* Dropdown toggle button */}
-                  <button
-                    onClick={() => setIsOpens(!isOpens)}
-                    className="relative z-10 "
-                  >
-                    <img
-                      className="flex-shrink-0 object-cover mx-1 rounded-full w-9 h-9"
-                      src="https://images.unsplash.com/photo-1523779917675-b6ed3a42a561?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8d29tYW4lMjBibHVlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=face&w=500&q=200"
-                      alt="jane avatar"
-                    />
-                  </button>
-
-                  {/* Dropdown menu */}
-                  {isOpens && (
-                    <div
-                      className="absolute right-0 z-20 w-56 py-2 mt-2 overflow-hidden origin-top-right bg-white rounded-md shadow-xl dark:bg-gray-800"
-                      onClick={() => setIsOpens(false)}
-                    >
-                      <a
-                        href="#"
-                        className="flex items-center p-3 -mt-2 text-sm text-gray-600 transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
-                      >
-                        <img
-                          className="flex-shrink-0 object-cover mx-1 rounded-full w-9 h-9"
-                          src="https://images.unsplash.com/photo-1523779917675-b6ed3a42a561?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8d29tYW4lMjBibHVlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=face&w=500&q=200"
-                          alt="jane avatar"
-                        />
-                        <div className="mx-1">
-                          <h1 className="text-sm font-semibold text-gray-700 dark:text-gray-200">
-                            Jane Doe
-                          </h1>
-                          <p className="text-sm text-gray-500 dark:text-gray-400">
-                            janedoe@exampl.com
-                          </p>
-                        </div>
-                      </a>
-
-                      <hr className="border-gray-200 dark:border-gray-700" />
-
-                      <a
-                        href="#"
-                        className="flex items-center p-3 text-sm text-gray-600 capitalize transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
-                      >
-                        <svg
-                          className="w-5 h-5 mx-1"
-                          viewBox="0 0 24 24"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            d="M7 8C7 5.23858 9.23858 3 12 3C14.7614 3 17 5.23858 17 8C17 10.7614 14.7614 13 12 13C9.23858 13 7 10.7614 7 8ZM12 11C13.6569 11 15 9.65685 15 8C15 6.34315 13.6569 5 12 5C10.3431 5 9 6.34315 9 8C9 9.65685 10.3431 11 12 11Z"
-                            fill="currentColor"
-                          ></path>
-                          <path
-                            d="M6.34315 16.3431C4.84285 17.8434 4 19.8783 4 22H6C6 20.4087 6.63214 18.8826 7.75736 17.7574C8.88258 16.6321 10.4087 16 12 16C13.5913 16 15.1174 16.6321 16.2426 17.7574C17.3679 18.8826 18 20.4087 18 22H20C20 19.8783 19.1571 17.8434 17.6569 16.3431C16.1566 14.8429 14.1217 14 12 14C9.87827 14 7.84344 14.8429 6.34315 16.3431Z"
-                            fill="currentColor"
-                          ></path>
-                        </svg>
-                        <span className="mx-1">View Profile</span>
-                      </a>
-                      <button className="mx-1  p-3  hover:bg-gray-100">
-                        Logout
-                      </button>
-                    </div>
-                  )}
+                <Button
+                  variant="outline"
+                  className="rounded-full mx-2 p-0 size-9 "
+                >
+                  <Search />
+                </Button>
+                <div className="ml-10">
+                  <DropdownMenu>
+                    <DropdownMenuTrigger>
+                      <Avatar>
+                        <AvatarImage src="https://github.com/shadcn.png" />
+                        <AvatarFallback>CN</AvatarFallback>
+                      </Avatar>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                      <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                      <DropdownMenuSeparator />
+                      <DropdownMenuItem>Profile</DropdownMenuItem>
+                      <DropdownMenuItem>Dashboard</DropdownMenuItem>
+                      <DropdownMenuItem>My Shop</DropdownMenuItem>
+                      <DropdownMenuSeparator />
+                      <DropdownMenuItem className="bg-red-500 text-gray-100 cursor-pointer">
+                        <LogOut />
+                        Log Out
+                      </DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
                 </div>
               </div>
             </div>
