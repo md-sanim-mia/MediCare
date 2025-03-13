d0a3a415669ec9242bcde154a59e12c14d058133
SanimSRk <hasansanim562@gmail.com>
2025-03-13 23:21:25 +0600

    work in add product

--- src/components/modules/Admin/AddProductForm.tsx
+++ src/components/modules/Admin/AddProductForm.tsx
@@ -16,8 +16,8 @@ import {
   useForm,
 } from "react-hook-form";
 import { Textarea } from "@/components/ui/textarea";
-import { useEffect, useState } from "react";
-import { Plus } from "lucide-react";
+import React, { useEffect, useState } from "react";
+import { Plus, X } from "lucide-react";
 
 import {
   Select,
@@ -28,6 +28,8 @@ import {
 } from "@/components/ui/select";
 import { useRouter } from "next/navigation";
 import { TMedicine } from "@/types/product";
+import ImageGenetors from "@/components/ui/core/NMImageGenertor";
+import Image from "next/image";
 
 export default function AddProductForm() {
   const router = useRouter();
@@ -38,12 +40,14 @@ export default function AddProductForm() {
       description: "",
       price: "",
       category: "",
+      expiry_date: "",
+      dosage: "",
       brand: "",
       stock: "",
-      weight: "",
-      availableColors: [{ value: "" }],
-      keyFeatures: [{ value: "" }],
-      specification: [{ key: "", value: "" }],
+      form: "",
+      prescription_required: "",
+      side_effects: [{ value: "" }],
+      uses: [{ value: "" }],
     },
   });
 
@@ -53,7 +57,7 @@ export default function AddProductForm() {
 
   const { append: appendColor, fields: colorFields } = useFieldArray({
     control: form.control,
-    name: "availableColors",
+    name: "side_effects",
   });
 
   const addColor = () => {
@@ -62,24 +66,23 @@ export default function AddProductForm() {
 
   const { append: appendFeatures, fields: featureFields } = useFieldArray({
     control: form.control,
-    name: "keyFeatures",
+    name: "uses",
   });
 
   const addFeatures = () => {
     appendFeatures({ value: "" });
   };
 
-  const { append: appendSpec, fields: specFields } = useFieldArray({
-    control: form.control,
-    name: "specification",
-  });
-
-  const addSpec = () => {
-    appendSpec({ key: "", value: "" });
+  const [productImage, setProductImage] = useState<string>();
+  const handileClickChengeImage = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const file = event.target.files![0];
+    const image = await ImageGenetors(file);
+    setProductImage(image);
   };
-
   // console.log(specFields);
-
+  console.log(productImage);
   return (
     <div className="border-2 border-gray-300 rounded-xl flex-grow max-w-2xl p-5 ">
       <div className="flex items-center space-x-4 mb-5 ">
@@ -134,10 +137,62 @@ export default function AddProductForm() {
             />
             <FormField
               control={form.control}
-              name="weight"
+              name="category"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel>Weight</FormLabel>
+                  <FormLabel>Category</FormLabel>
+                  <FormControl>
+                    <Input {...field} value={field.value || ""} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="dosage"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Dosage</FormLabel>
+                  <FormControl>
+                    <Input {...field} value={field.value || ""} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="expiry_date"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Expiry Date</FormLabel>
+                  <FormControl>
+                    <Input {...field} value={field.value || ""} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="form"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Form</FormLabel>
+                  <FormControl>
+                    <Input {...field} value={field.value || ""} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="prescription_required"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Prescription Required</FormLabel>
                   <FormControl>
                     <Input {...field} value={field.value || ""} />
                   </FormControl>
@@ -171,7 +226,42 @@ export default function AddProductForm() {
             <div className="flex justify-between items-center border-t border-b py-3 my-5">
               <p className="text-primary font-bold text-xl">Images</p>
             </div>
-            <div className="flex gap-4 "></div>
+            <div className="flex gap-4 ">
+              <div className="flex flex-col items-center w-full gap-4">
+                <Input
+                  id="image-upload"
+                  type="file"
+                  accept="image/*"
+                  className="hidden"
+                  onChange={handileClickChengeImage}
+                />
+                <label
+                  htmlFor="image-upload"
+                  className="w-full h-36 md:size-36 flex items-center justify-center border-2 border-dashed border-gray-300 rounded-md cursor-pointer text-center text-sm text-gray-500 hover:bg-gray-50 transition"
+                >
+                  Upload Image
+                </label>
+              </div>
+              {productImage && (
+                <div className="relative w-36 h-36 rounded-md overflow-hidden border border-dashed border-gray-300">
+                  {/* <Image
+                    width={500}
+                    height={500}
+                    src={productImage}
+                    alt={`Preview`}
+                    className="object-cover w-full h-full"
+                  /> */}
+                  <img src={productImage} alt="" />
+                  <Button
+                    type="button"
+                    size="sm"
+                    className="bg-red-300 hover:bg-red-400 absolute -top-0 -right-0 w-6 h-6 p-0 rounded-full"
+                  >
+                    <X className="w-4 h-4" />
+                  </Button>
+                </div>
+              )}
+            </div>
           </div>
 
           <div>
@@ -192,10 +282,10 @@ export default function AddProductForm() {
                 <div key={colorField.id}>
                   <FormField
                     control={form.control}
-                    name={`availableColors.${index}.value`}
+                    name={`side_effects.${index}.value`}
                     render={({ field }) => (
                       <FormItem>
-                        <FormLabel>Color {index + 1}</FormLabel>
+                        <FormLabel> Side Effect {index + 1}</FormLabel>
                         <FormControl>
                           <Input {...field} value={field.value || ""} />
                         </FormControl>
@@ -223,13 +313,13 @@ export default function AddProductForm() {
 
             <div className="my-5">
               {featureFields.map((featureField, index) => (
-                <div key={featureField.id}>
+                <div className="my-3" key={featureField.id}>
                   <FormField
                     control={form.control}
-                    name={`keyFeatures.${index}.value`}
+                    name={`uses.${index}.value`}
                     render={({ field }) => (
                       <FormItem>
-                        <FormLabel>Key Feature {index + 1}</FormLabel>
+                        <FormLabel> Use Case {index + 1}</FormLabel>
                         <FormControl>
                           <Input {...field} value={field.value || ""} />
                         </FormControl>
@@ -241,55 +331,6 @@ export default function AddProductForm() {
               ))}
             </div>
           </div>
-
-          <div>
-            <div className="flex justify-between items-center border-t border-b py-3 my-5">
-              <p className="text-primary font-bold text-xl">Specification</p>
-              <Button
-                onClick={addSpec}
-                variant="outline"
-                className="size-10"
-                type="button"
-              >
-                <Plus className="text-primary" />
-              </Button>
-            </div>
-
-            {specFields.map((specField, index) => (
-              <div
-                key={specField.id}
-                className="grid grid-cols-1 gap-4 md:grid-cols-2 my-5"
-              >
-                <FormField
-                  control={form.control}
-                  name={`specification.${index}.key`}
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Feature name {index + 1}</FormLabel>
-                      <FormControl>
-                        <Input {...field} value={field.value || ""} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name={`specification.${index}.value`}
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Feature Description {index + 1}</FormLabel>
-                      <FormControl>
-                        <Input {...field} value={field.value || ""} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-            ))}
-          </div>
-
           <Button type="submit" className="mt-5 w-full" disabled={isSubmitting}>
             {isSubmitting ? "Adding Product....." : "Add Product"}
           </Button>
